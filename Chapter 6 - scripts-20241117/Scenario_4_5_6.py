a1a5fe262b2c58cc01595d812cc6e646d390af59
bropenguin847 <bropenguin847@gmail.com>
2024-11-25 23:16:28 +0800

    minor changes

--- Chapter 6 - scripts-20241117/Scenario_4_5_6.py
+++ Chapter 6 - scripts-20241117/Scenario_4_5_6.py
@@ -1,9 +1,37 @@
-# Scenario 5: Customer Feedback Analysis
 import pandas as pd
 import os
 
 current_dir = os.path.dirname(__file__)
 
+# Scenario 4: Monthly Expense Tracker
+expenses_path = os.path.join(current_dir, "monthly_expenses.xlsx")
+
+dataframe = pd.read_excel(expenses_path, sheet_name = "Expenses")
+expenses = dataframe.to_numpy()
+
+expenses_type = []
+savings_oppurtunity = []
+
+def categorize_expense(amount):
+    return "High" if amount > 500 else "Low"
+
+for i in range(len(expenses)):
+    expenses_type.append(categorize_expense(expenses[i,2]))
+    if expenses[i, 1] == "Entertainment":
+        savings_oppurtunity.append("Yes")
+    else:
+        savings_oppurtunity.append("No")
+
+# Add bonus and status column to original dataframe
+dataframe["Expenses Type"] = expenses_type
+dataframe["Savings Oppurtunity"] = savings_oppurtunity
+
+# Write updated df to excel file
+with pd.ExcelWriter(expenses_path, mode='a', if_sheet_exists='overlay') as writer:
+    dataframe.to_excel(writer, sheet_name = "Expenses", index=False, header=True)
+
+##########################################################################################
+# Scenario 5: Customer Feedback Analysis
 customer_path = os.path.join(current_dir, "customer_feedback.xlsx")
 feedback = pd.read_excel(customer_path, sheet_name = "Feedback")
 
--- Chapter 6 - scripts-20241117/trycoding.py
+++ Chapter 6 - scripts-20241117/trycoding.py
@@ -40,40 +40,4 @@
 #     output_data_2= pd.DataFrame([status_list]).T
 #     output_data_2.to_excel(writer, sheet_name='Details', startcol=5, startrow=0, index=False, header=False)
 
-#############################################################################
-import os
-import pandas as pd
 
-directory = os.path.dirname(__file__)
-expenses_path = os.path.join(directory, "monthly_expenses.xlsx")
-
-dataframe = pd.read_excel(expenses_path, sheet_name="Expenses")
-expenses = dataframe.to_numpy()
-
-expenses_type = []
-savings_oppurtunity = []
-
-def categorize_expense(self):
-    return "High" if self > 500 else "Low"
-
-for i in range(len(expenses)):
-    expenses_type.append(categorize_expense(expenses[i,0]))
-    if expenses[x,x] == "Entertainment":
-        
-
-
-for i in range(len(data)):
-    adjusted_price.append(adjust_price(data[i,2],data[i,3])) 
-    if data[i,4] >0:
-        stock.append("In Stock")
-    else:
-        stock.append("Out of stock")
-   
-
-# Add bonus and status column to original df
-df['Adjusted price']=adjusted_price
-df['In/Out of stock']=stock
-
-# Write updated df to excel file
-with pd.ExcelWriter('product_catalog.xlsx', mode='a', if_sheet_exists='overlay') as writer:
-    df.to_excel(writer, sheet_name='Products', index=False, header=True)
