939390df7087ffbbafce40594dd6b485317eb4bf
bropenguin847 <bropenguin847@gmail.com>
2024-12-25 23:36:27 +0800

    final_convo

--- Chapter 8 Graphics/Signals/convolution.py
+++ Chapter 8 Graphics/Signals/convolution.py
@@ -8,6 +8,12 @@ class waveform:
         self.center = (t2-t1)/2
 t = np.linspace(-2, 3, 1000)
 
+def graphlabel(title, xlabel, ylabel):
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+
 # Task 1
 # Define triangle
 triangle = waveform(t, 0, 2)
@@ -31,52 +37,40 @@ for i, time in enumerate(square.time):
         x2[i] = 1
 
 # z(t) = x1(t) + x2(t)
-fig3 = x1 + x2
+fig1c = x1 + x2
 
 yt = np.convolve(x1, x2, mode='full')
 t_conv = np.linspace(triangle.time[0] + square.time[0], triangle.time[-1] + square.time[-1], len(yt))
-#################################################
+#############
 plt.subplot(3,2,1)
-plt.plot(t, x1, '-b')
-plt.title('Figure 1 (a)') 
-plt.xlabel('Time (seconds)') 
-plt.ylabel('Triangle, x1')
-plt.grid(True)
+plt.plot(t, x1, color='mediumblue')
+graphlabel('Figure 1 (a)', 'Time (seconds)', 'Triangle, x1')
 plt.axis([-2, 3, 0, 2])
 
 plt.subplot(322)
-plt.plot(t, x2, 'r')
-plt.title('Figure 2 (b)') 
-plt.xlabel('Time (seconds)')
-plt.ylabel('Rectangle, x2')
-plt.grid(True)
+plt.plot(t, x2, 'firebrick')
+graphlabel('Figure 1 (b)', 'Time (seconds)', 'Rectangle, x2')
 plt.axis([-2, 3, 0, 2])
 
 plt.subplot(3, 2, 3)
-plt.plot(t, x2, '-r', t, x1, 'b')
-plt.grid(True)
+plt.plot(t, x2, 'crimson', t, x1, 'navy')
+graphlabel('Overlap', 'Time (seconds)', '')
 plt.axis([-2, 3, 0, 2])
 
 plt.subplot(3, 2, 4)
-plt.plot(t, fig3, 'm')
-plt.title('Figure 3 (c)') 
-plt.xlabel('Time (seconds)')
-plt.ylabel('z')
+plt.plot(t, fig1c, 'deepskyblue')
+graphlabel('Figure 1 (c)', 'Time (seconds)', 'z')
 plt.axis([-2, 3, 0, 4])
-plt.grid(True)
 
 plt.subplot(3,2,(5,6))
-plt.plot(t_conv, yt, color='green')
-plt.title('Figure 4 (d)')
-plt.xlabel('Time (seconds)')
-plt.ylabel('Convolution, y')
-plt.grid(True)
+plt.plot(t_conv, yt, color='forestgreen')
+graphlabel('Figure 1 (d)', 'Time (seconds)', 'Convolution, y')
 plt.axis([-3, 6, 0, 500])
 
 plt.tight_layout()
 plt.show()
 
-
+#################################################
 # Task 2
 slant = waveform(t, -1, 1)
 g = np.zeros_like(t)
@@ -87,11 +81,44 @@ for i, time in enumerate(slant.time):
         g[i] = 1 * (time +1)
     else:
         g[i] = 0
+      
+# Define square
+square1 = waveform(t, -1, 0)
+square2 = waveform(t, 1, 2)
 
-plt.plot(t, g)
-plt.title('Figure 2 (a)')
-plt.xlabel('Time (seconds)')
-plt.ylabel('Triangle, g')
-plt.grid(True)
-plt.axis([-2, 2, 0, 3])
+for i, time in enumerate(t):
+    if square1.t1 <= time <= square1.t2:
+        h[i] = 2
+        
+    if square2.t1 <= time <= square2.t2:
+        h[i] = 1
+
+# yt = gt + ht
+fig2c= g + h
+        
+yt = np.convolve(g, h, mode='full')
+#t_conv = np.linspace(square1.time[0] + slant.time[0], square2.time[-1] + slant.time[-1], len(yt))
+t_conv = np.linspace(-4,4, len(yt))
+###################
+plt.subplot(2,2,1)
+plt.plot(t, g, color='midnightblue')
+graphlabel('Figure 2 (a)', 'Time (seconds)', 'Triangle, g')
+plt.axis([-2, 3, 0, 3])
+
+plt.subplot(222)
+plt.plot(t, h, color='dodgerblue')
+graphlabel('Figure 2 (b)', 'Time (seconds)', 'Rectangle, h')
+plt.axis([-2, 3, 0, 3])
+
+plt.subplot(223)
+plt.plot(t, fig2c, color='cornflowerblue')
+graphlabel('Figure 2 (c)', 'Time (seconds)', 'z')
+plt.axis([-2, 3, 0, 3])
+
+plt.subplot(224)
+plt.plot(t_conv, yt, color='mediumseagreen')
+graphlabel('Figure 2 (d)', 'Time (seconds)', 'Convolution, y')
+plt.axis([-3, 4, 0, 1000])
+
+plt.tight_layout()
 plt.show()
