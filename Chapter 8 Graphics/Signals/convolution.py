0ecc03a567e0a36246d959a6ca00a284e360897b
bropenguin847 <bropenguin847@gmail.com>
2024-12-29 16:48:52 +0800

    adjusted for sampling rate

--- Chapter 8 Graphics/Signals/convolution.py
+++ Chapter 8 Graphics/Signals/convolution.py
@@ -8,21 +8,20 @@ class waveform:
         self.center = (t2-t1)/2
 t = np.linspace(-2, 3, 500)
 
-def graphlabel(title, xlabel, ylabel):
+def graph_label(title, xlabel, ylabel):
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.grid(True)
 
 # Task 1
-# Define triangle
+# Define triangle & rectangle
 triangle = waveform(t, 0, 2)
-x1 = np.zeros_like(triangle.time)      # Initialize with zero first
-# Define square
+x1 = np.zeros_like(t)      # Initialize with zero first
 square = waveform(t, -1, 1)
-x2 = np.zeros_like(square.time)
+x2 = np.zeros_like(t)
 
-for i, time in enumerate(triangle.time):
+for i, time in enumerate(t):
     # Rising part
     if triangle.t1 <= time <= triangle.center:
         x1[i] = 2 * time
@@ -30,55 +29,47 @@ for i, time in enumerate(triangle.time):
     elif triangle.center < time <= triangle.t2:
         x1[i] = 2 * (2 - time)
 
-for i, time in enumerate(square.time):
+for i, time in enumerate(t):
     x2[i] = 0
     if square.t1 <= time <= square.t2:
         x2[i] = 1
 
 # z(t) = x1(t) + x2(t)
-fig1c = x1 + x2
+z = x1 + x2
 
-# yt = np.convolve(x1, x2, mode='full')
-yt = np.convolve(x1, x2, mode='full')
-t_conv = np.linspace(triangle.time[0] + square.time[0], triangle.time[-1] + square.time[-1], len(yt))
-#############
+y = np.convolve(x1, x2, mode='full') / 100    # Adjust for sampling rate by dividing 100
+t_conv = np.linspace(triangle.time[0] + square.time[0], triangle.time[-1] + square.time[-1], len(y))
+
 plt.subplot(3,2,1)
-plt.plot(t, x1, color='mediumblue')
-graphlabel('Figure 1 (a)', 'Time (seconds)', 'Triangle, x1')
-plt.axis([-2, 3, 0, 2])
+plt.plot(t, x1, 'mediumblue')
+graph_label('Figure 1 (a)', 'Time (seconds)', 'Triangle, x1')
+plt.axis([-2, 3, 0, 3])
 
 plt.subplot(322)
 plt.plot(t, x2, 'firebrick')
-graphlabel('Figure 1 (b)', 'Time (seconds)', 'Rectangle, x2')
-plt.axis([-2, 3, 0, 2])
+graph_label('Figure 1 (b)', 'Time (seconds)', 'Rectangle, x2')
+plt.axis([-2, 3, 0, 3])
 
 plt.subplot(3, 2, 3)
-plt.plot(t, x2, 'crimson', label= 'x1')
-plt.plot(t, x1, 'navy', label= 'x2')
-graphlabel('Overlap', 'Time (seconds)', '')
-plt.legend(loc='upper left')
-plt.axis([-2, 3, 0, 2])
+plt.plot(t, z, 'deepskyblue')
+graph_label('Figure 1 (c)', 'Time (seconds)', 'z')
+plt.axis([-2, 3, 0, 3])
 
-
-plt.subplot(3, 2, 4)
-plt.plot(t, fig1c, 'deepskyblue')
-graphlabel('Figure 1 (c)', 'Time (seconds)', 'z')
-plt.axis([-2, 3, 0, 4])
-
-plt.subplot(3,2,(5,6))
-plt.plot(t_conv, yt, 'forestgreen')
-graphlabel('Figure 1 (d)', 'Time (seconds)', 'Convolution, y')
-plt.axis([-2, 6, 0, 200])
+plt.subplot(3,2,4)
+plt.plot(t_conv, y, 'forestgreen')
+graph_label('Figure 1 (d)', 'Time (seconds)', 'Convolution, y')
+plt.axis([-2, 6, 0, 3])
 
 plt.tight_layout()
 plt.show()
 
-#################################################
+#####################################################
 # Task 2
 slant = waveform(t, -1, 1)
 g = np.zeros_like(t)
 h = np.zeros_like(t)
 
+# Define slant
 for i, time in enumerate(slant.time):
     if slant.t1 <= time <= slant.t2:
         g[i] = 1 * (time +1)
@@ -96,32 +87,31 @@ for i, time in enumerate(t):
     if square2.t1 <= time <= square2.t2:
         h[i] = 1
 
-# yt = gt + ht
-fig2c= g + h
+# zt = gt + ht
+z = g + h
         
-yt = np.convolve(g, h, mode='full')
-#t_conv = np.linspace(square1.time[0] + slant.time[0], square2.time[-1] + slant.time[-1], len(yt))
-t_conv = np.linspace(-4,4, len(yt))
-###################
+y = np.convolve(g, h, mode='full') / 100    # Adjust for sampling rate by dividing 100
+t_conv = np.linspace(square1.time[0] + slant.time[0], square2.time[-1] + slant.time[-1], len(y))
+
 plt.subplot(2,2,1)
 plt.plot(t, g, 'blueviolet')
-graphlabel('Figure 2 (a)', 'Time (seconds)', 'Triangle, g')
+graph_label('Figure 2 (a)', 'Time (seconds)', 'Triangle, g')
 plt.axis([-2, 3, 0, 3])
 
 plt.subplot(222)
 plt.plot(t, h, 'mediumvioletred')
-graphlabel('Figure 2 (b)', 'Time (seconds)', 'Rectangle, h')
+graph_label('Figure 2 (b)', 'Time (seconds)', 'Rectangle, h')
 plt.axis([-2, 3, 0, 3])
 
 plt.subplot(223)
-plt.plot(t, fig2c, 'cornflowerblue')
-graphlabel('Figure 2 (c)', 'Time (seconds)', 'z')
+plt.plot(t, z, 'cornflowerblue')
+graph_label('Figure 2 (c)', 'Time (seconds)', 'z')
 plt.axis([-2, 3, 0, 3])
 
 plt.subplot(224)
-plt.plot(t_conv, yt, 'mediumseagreen')
-graphlabel('Figure 2 (d)', 'Time (seconds)', 'Convolution, y')
-plt.axis([-3, 4, 0, 400])
+plt.plot(t_conv, y, 'mediumseagreen')
+graph_label('Figure 2 (d)', 'Time (seconds)', 'Convolution, y')
+plt.axis([-4, 4, 0, 3])
 
 plt.tight_layout()
 plt.show()
